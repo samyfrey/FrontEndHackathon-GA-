92f20c1b768ab2028c88dac51616b4c41a3f5bf4
samyfrey <samyfrey@yahoo.com>
2022-03-03 15:14:23 -0500

    flex wrap working by setting fixed  width and height on all img

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -2,7 +2,7 @@
 export const PhotoGallery = ({item: {height, flexGrow, width, urlImg }}) => {
     return (
             <div>
-            <img src={`${urlImg}`} alt='' style={{width: `${width}`, height: `${height}`, flexGrow: `${flexGrow}`}}/>
+            <img src={`${urlImg}`} alt='' style={{width: `${width}`, height: `${height}`, flexGrow: `${flexGrow}`, objectFit: 'cover'}}/>
             </div>
 
       
