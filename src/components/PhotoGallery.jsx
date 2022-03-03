525790905e3cd09c331fe9095758fb47a0308c39
samyfrey <samyfrey@yahoo.com>
2022-03-03 15:50:47 -0500

    cleaned up gallery component

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,8 +1,19 @@
 
 export const PhotoGallery = ({item: {height, flexGrow, width, urlImg }}) => {
+
+    const imageStyle = {
+        width: `${width}`, 
+        height: `${height}`, 
+        flexGrow: `${flexGrow}`, 
+        objectFit: 'cover',
+        border: '5px solid white',
+
+
+    }
+
     return (
             <div>
-            <img src={`${urlImg}`} alt='' style={{width: `${width}`, height: `${height}`, flexGrow: `${flexGrow}`, objectFit: 'cover'}}/>
+            <img src={`${urlImg}`} alt='' style={imageStyle}/>
             </div>
 
       
--- src/components/styles/GalleryContainer.js
+++ src/components/styles/GalleryContainer.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 export const GalleryContainer = styled.div`
-    width: 80%;
-    border: 5px solid green;
+    width: 900px;
+    // border: 5px solid green;
+    // background: grey;
     margin: auto;
     display: flex;
     flex-wrap: wrap;
-    background: grey;
     object-fit: cover;
     justify-content: center
     
