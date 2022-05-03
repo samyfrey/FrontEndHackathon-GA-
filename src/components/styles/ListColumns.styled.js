ca4e3bdd3230d0efd41c264222f595b920152090
samyfrey <samyfrey@yahoo.com>
2022-05-02 20:03:52 -0400

    fixed menu responsive

--- src/components/styles/ListColumns.styled.js
+++ src/components/styles/ListColumns.styled.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components'
 
 export const ListColumns = styled.ul`
-    display: flex; 
-    flex-wrap: wrap;
-    width: 50%;
-    padding-inline-start: 0px;
-    font-size: 15px;
-    // width: 50%;
+	display: flex;
+	flex-wrap: wrap;
+	width: 50%;
+	padding-inline-start: 0px;
+	font-size: 15px;
+	// width: 50%;
 
-    // & > div,
-    & > ul {
-        flex: 1;
-    }
+	// & > div,
+	& > ul {
+		flex: 1;
+	}
 
+	@media (max-width: ${({ theme }) => theme.mobile}) {
+		flex-direction: column;
+        align-items: center;
+	}
 `
